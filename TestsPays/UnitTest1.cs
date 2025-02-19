c35657f086cbae1054fd505ad7ee6bee7cb9bf76
Qwatrinet <quentin.watrinet@gmail.com>
2025-02-19 11:12:25 +0100

    Update UnitTest1.cs

--- TestsPays/UnitTest1.cs
+++ TestsPays/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace TestsPays
 
             country.CodePays = "FR";
 
-            Assert.AreEqual("BOBBY", country.CodePays);
+            Assert.AreEqual("FR", country.CodePays);
         }
     }
 }
